25503e18ba7a943407a710735e592aa708726b05
Victor Lira <victorliraradio@gmail.com>
2023-04-20 13:58:19 -0300

    DOM included

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-let randomNumber = Math.random() * 6;
-console.log(randomNumber);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+let imagesTag = document.querySelectorAll("div img");
+
+imagesTag[0].setAttribute("src", `/Dicee/images/dice${randomNumber1}.png`);
+imagesTag[1].setAttribute("src", `/Dicee/images/dice${randomNumber2}.png`);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "Player 1 WINS!!!";
+}
+if (randomNumber1 == randomNumber2) {
+  document.querySelector("h1").textContent = "DRAW!!!";
+}
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").textContent = "Player 2 WINS!!!";
+}
