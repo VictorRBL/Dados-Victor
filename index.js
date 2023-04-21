11143d2fe0ac13f77298a8bb04fbaab3c1246d5e
Victor Lira <victorliraradio@gmail.com>
2023-04-20 21:35:34 -0300

    images in the folder

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 let imagesTag = document.querySelectorAll("div img");
 
-imagesTag[0].setAttribute("src", `/Dicee/images/dice${randomNumber1}.png`);
-imagesTag[1].setAttribute("src", `/Dicee/images/dice${randomNumber2}.png`);
+imagesTag[0].setAttribute("src", `images/dice${randomNumber1}.png`);
+imagesTag[1].setAttribute("src", `images/dice${randomNumber2}.png`);
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").textContent = "Player 1 WINS!!!";
